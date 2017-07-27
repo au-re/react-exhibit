db6577e01ce05bfc403dbc823c09b8b159af5e43
aure <au.re@live.com>
2017-07-27 23:53:53 +0200

    test(Header): Fix test issues with react router

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -1,9 +1,12 @@
 import { Header } from "../index";
 import React from "react";
 import ReactDOM from "react-dom";
+import { MemoryRouter as Router } from "react-router-dom";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
 
-  ReactDOM.render(<Header />, div);
+  ReactDOM.render(<Router>
+    <Header />
+  </Router>, div);
 });
