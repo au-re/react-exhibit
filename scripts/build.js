b4dc086ab769ce40d38668e13dd4211907baa157
aure <au.re@live.com>
2017-07-27 22:23:07 +0200

    fix(package): Minor polishing fixes to the logs

--- scripts/build.js
+++ scripts/build.js
@@ -1,26 +1,28 @@
+/* eslint-disable no-console */
+
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+process.env.BABEL_ENV = "production";
+process.env.NODE_ENV = "production";
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
+process.on("unhandledRejection", err => {
   throw err;
 });
 
 // Ensure environment variables are read.
-require('../config/env');
+require("../config/env");
 
-const path = require('path');
-const chalk = require('chalk');
-const fs = require('fs-extra');
-const webpack = require('webpack');
-const config = require('../config/webpack.config.prod');
-const paths = require('../config/paths');
-const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
-const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
-const FileSizeReporter = require('react-dev-utils/FileSizeReporter');
+const path = require("path");
+const chalk = require("chalk");
+const fs = require("fs-extra");
+const webpack = require("webpack");
+const config = require("../config/webpack.config.prod");
+const paths = require("../config/paths");
+const checkRequiredFiles = require("react-dev-utils/checkRequiredFiles");
+const formatWebpackMessages = require("react-dev-utils/formatWebpackMessages");
+const FileSizeReporter = require("react-dev-utils/FileSizeReporter");
 
 const measureFileSizesBeforeBuild =
   FileSizeReporter.measureFileSizesBeforeBuild;
@@ -49,45 +51,48 @@ measureFileSizesBeforeBuild(paths.appDist)
     return build(previousFileSizes);
   })
   .then(
-    ({ stats, previousFileSizes, warnings }) => {
-      if (warnings.length) {
-        console.log(chalk.yellow('Compiled with warnings.\n'));
-        console.log(warnings.join('\n\n'));
-        console.log(
-          '\nSearch for the ' +
-            chalk.underline(chalk.yellow('keywords')) +
-            ' to learn more about each warning.'
-        );
-        console.log(
-          'To ignore, add ' +
-            chalk.cyan('// eslint-disable-next-line') +
-            ' to the line before.\n'
-        );
-      } else {
-        console.log(chalk.green('Compiled successfully.\n'));
-      }
-
-      console.log('File sizes after gzip:\n');
-      printFileSizesAfterBuild(
-        stats,
-        previousFileSizes,
-        paths.appDist,
-        WARN_AFTER_BUNDLE_GZIP_SIZE,
-        WARN_AFTER_CHUNK_GZIP_SIZE
+  ({ stats, previousFileSizes, warnings }) => {
+    if (warnings.length) {
+      console.log(chalk.yellow("Compiled with warnings.\n"));
+      console.log(warnings.join("\n\n"));
+      console.log(
+        "\nSearch for the " +
+        chalk.underline(chalk.yellow("keywords")) +
+        " to learn more about each warning."
       );
-      console.log();
-      console.log(chalk.green('You can now publish your library to npm'))
+      console.log(
+        "To ignore, add " +
+        chalk.cyan("// eslint-disable-next-line") +
+        " to the line before.\n"
+      );
+    } else {
+      console.log(chalk.green("Compiled successfully.\n"));
+    }
+
+    console.log("File sizes after gzip:\n");
+    printFileSizesAfterBuild(
+      stats,
+      previousFileSizes,
+      paths.appDist,
+      WARN_AFTER_BUNDLE_GZIP_SIZE,
+      WARN_AFTER_CHUNK_GZIP_SIZE
+    );
+    console.log();
+    console.log(`Your library is now built at ${chalk.cyan(paths.appDist)}`);
+    console.log(`To publish it to ${chalk.green("npm")}, run:`);
+    console.log();
+    console.log(`  ${chalk.cyan("npm")} publish`);
     },
     err => {
-      console.log(chalk.red('Failed to compile.\n'));
-      console.log((err.message || err) + '\n');
+      console.log(chalk.red("Failed to compile.\n"));
+      console.log((err.message || err) + "\n");
       process.exit(1);
     }
   );
 
 // Create the production build and print the deployment instructions.
 function build(previousFileSizes) {
-  console.log('Creating an optimized production build...');
+  console.log("Creating an optimized production build...");
 
   let compiler = webpack(config);
   return new Promise((resolve, reject) => {
@@ -97,21 +102,21 @@ function build(previousFileSizes) {
       }
       const messages = formatWebpackMessages(stats.toJson({}, true));
       if (messages.errors.length) {
-        return reject(new Error(messages.errors.join('\n\n')));
+        return reject(new Error(messages.errors.join("\n\n")));
       }
       if (
         process.env.CI &&
-        (typeof process.env.CI !== 'string' ||
-          process.env.CI.toLowerCase() !== 'false') &&
+        (typeof process.env.CI !== "string" ||
+          process.env.CI.toLowerCase() !== "false") &&
         messages.warnings.length
       ) {
         console.log(
           chalk.yellow(
-            '\nTreating warnings as errors because process.env.CI = true.\n' +
-              'Most CI servers set it automatically.\n'
+            "\nTreating warnings as errors because process.env.CI = true.\n" +
+              "Most CI servers set it automatically.\n"
           )
         );
-        return reject(new Error(messages.warnings.join('\n\n')));
+        return reject(new Error(messages.warnings.join("\n\n")));
       }
       return resolve({
         stats,
--- src/Showcase/index.js
+++ src/Showcase/index.js
@@ -3,7 +3,15 @@ import "./Showcase.css";
 import { PrismCode } from "react-prism";
 import React from "react";
 
-const Showcase = ({source, demo}) => (
+/**
+ * A showcase component, renders a react component and displays source code.
+ *
+ * @export
+ * @param {string} demo - the react demo to be run
+ * @param {string} source - the source code to be displayed
+ * @return {object} Showcase Component
+ */
+const Showcase = ({ source, demo }) => (
   <div className="Showcase">
     <div className="Showcase__Demo">
       {demo}
