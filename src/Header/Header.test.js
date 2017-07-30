55c168f77f73d243e245bc7f8e90e1362774a7eb
aure <au.re@live.com>
2017-07-30 17:43:06 +0200

    fix(Header): Fix header styles

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -6,7 +6,13 @@ import { MemoryRouter as Router } from "react-router-dom";
 it("renders without crashing", () => {
   const div = document.createElement("div");
 
-  ReactDOM.render(<Router>
-    <Header />
-  </Router>, div);
+  ReactDOM.render(
+    <Router>
+      <Header />
+    </Router>, div);
+
+  ReactDOM.render(
+    <Router>
+      <Header label="Exhibit" url="TEST" />
+    </Router>, div);
 });
--- src/Header/index.js
+++ src/Header/index.js
@@ -4,15 +4,20 @@ import { Link } from "react-router-dom";
 import React from "react";
 
 /**
- * A simple header with a label that links to "/"
+ * A simple header with a label
  *
  * @export
- * @param {string} label - label to be displayed on the header
+ * @param {string} [label] - label to be displayed on the header
+ * @param {string} [url="{process.env.PUBLIC_URL}/"] - the url the header label links to
  * @return {object} - Header
  */
-const Header = ({ label, children }) => (
+const Header = ({ label, children, url=`${process.env.PUBLIC_URL}/` }) => (
   <div className="Header">
-    <Link className="Header__Label" to={`${process.env.PUBLIC_URL}/`}> {label} </Link>
+    <Link
+      className="Header__Label"
+      to={url}>
+      {label}
+    </Link>
   </div>);
 
 export default Header;
