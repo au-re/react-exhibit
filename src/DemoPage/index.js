a218f4df5907de34a234ad034dd4e6c38b18c50b
aure <au.re@live.com>
2017-08-13 10:38:55 +0200

    feat(DemoPages): Replace relative paths, with the library name
    In the component showcases, the source code will now display the library name instead of any
    relative path to index.js

--- src/DemoPage/index.js
+++ src/DemoPage/index.js
@@ -7,12 +7,31 @@ import Helmet from "react-helmet";
 import React from "react";
 
 /**
+ * The source code contains relative paths, replace them with an absolute path
+ * to the library name.
+ * @example
+ * parseDependency('import X from "../../index.js"');
+ * > 'import X from "libraryName"'
+ *
+ * @param {string} source - the source code
+ * @param {string} libraryName - the name of the library
+ * @returns {string} source where the dependency is replaced the libraryName
+ */
+function parseDependency(source, libraryName) {
+  return source.replace(/(\.\.\/)+index(.js)?/g, libraryName);
+}
+
+/**
  * Page containing the documentation and showcases
  *
- * @param {object} props - { docs, sources, demos }
+ * @param {string} name - Name of the component to be demoed
+ * @param {string} docs - jsdoc description of the component
+ * @param {string} sources - demo source code to be displayed
+ * @param {string} demos - component demos to be run
+ * @param {string} libraryName - name of the library, used to replace relative paths
  * @returns {object} - The showcases and docs for a component
  */
-function DemoPage({ name, docs, sources, demos }) {
+function DemoPage({ name, docs, sources, demos, libraryName }) {
 
   const componentDocs = docs.map((doc, idx) => (
     <Docs
@@ -24,7 +43,11 @@ function DemoPage({ name, docs, sources, demos }) {
   const showcases = sources.map((source, idx) => {
     let demo = (<div></div>);
     if (typeof demos[idx].default === "function") demo = demos[idx].default();
-    return (<Showcase key={idx} demo={demo} source={source} />);
+    return (
+      <Showcase
+        key={idx}
+        demo={demo}
+        source={parseDependency(source, libraryName)} />);
   });
 
   return (
@@ -58,6 +81,7 @@ function App({ components, label, readme }) {
         <Helmet><title>{`${label} - ${component}`}</title></Helmet>
         <DemoPage
           name={component}
+          libraryName={label}
           docs={components[component].docs}
           sources={components[component].source}
           demos={components[component].demo} />
